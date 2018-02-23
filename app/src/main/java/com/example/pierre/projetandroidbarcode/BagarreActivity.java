152e22d300f0f8bbae3edb7eb0d9f3dc780f326b
Pierre Tuaillon <pierre.tuaillon@etu.unice.fr>
2018-02-23 18:46:57 +0100

    Ajout de la baggarre à tester

--- app/src/main/java/com/example/pierre/projetandroidbarcode/BagarreActivity.java
+++ app/src/main/java/com/example/pierre/projetandroidbarcode/BagarreActivity.java
@@ -19,7 +19,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -30,8 +32,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Random;
 import java.util.UUID;
 
 import static android.os.Message.obtain;
@@ -41,314 +45,145 @@ import static android.provider.Settings.NameValueTable.NAME;
  * Created by natha on 05/02/2018.
  */
 
-public class BagarreActivity extends AppCompatActivity implements View.OnClickListener {
+public class BagarreActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private final static int REQUEST_CODE_ENABLE_BLUETOOTH = 0;
-    private BluetoothAdapter bluetoothAdapter;
-    private UUID myUuid;
-    private static final String TAG = "MY_APP_DEBUG_TAG";
-    private Handler mHandler; // handler that gets info from Bluetooth service
-    private List<BluetoothDevice> devices;
-    private TextView fightTextView;
-    private ImageView monstreView;
+    //Joueur
+    Monstre monstreJoueur;
+    Arme armeJoueur;
+    Armure armureJoueur;
 
-    // Defines several constants used when transmitting messages between the
-    // service and the UI.
-    private interface MessageConstants {
-        public static final int MESSAGE_READ = 0;
-        public static final int MESSAGE_WRITE = 1;
-        public static final int MESSAGE_TOAST = 2;
+    //Adversaire
+    Monstre monstreAFaireCombattre;
+    Arme armeMonstreAFaireCombattre;
+    Armure armureMonstreAFaireCombattre;
 
-        // ... (Add other message types here as needed.)
-    }
-
+    private TextView infoView, nomMonstreJoueur, nomMonstreAdversaire;
+    private ProgressBar barPVJoueur, barPVadversaire;
+    Button attaqueBTN;
 
     @Override
     public void onCreate(Bundle state) {
         super.onCreate(state);
+        setContentView(R.layout.bagarre);
 
-        fightTextView = (TextView) findViewById(R.id.fight);
-        monstreView = (ImageView) findViewById(R.id.monstre);
+        this.attaqueBTN = (Button) findViewById(R.id.buttonBagarre);
+        this.attaqueBTN.setOnClickListener(this);
 
-        TelephonyManager tManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
-        try {
-            myUuid = UUID.nameUUIDFromBytes(tManager.getDeviceId().getBytes("utf8"));
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
+        if(!alreadyInstance()) {
+            //On change le max des progressbar
+            this.barPVJoueur.setMax(monstreJoueur.getPDV());
+            this.barPVadversaire.setMax(monstreAFaireCombattre.getPDV());
 
-        //ask4Blu();
-
-        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (!bluetoothAdapter.isEnabled()) {
-            bluetoothAdapter.enable();
-        }
-        bluetoothAdapter.startDiscovery();
-
-        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        registerReceiver(bluetoothReceiver, filter);
-        Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
-        startActivity(discoverableIntent);
-
-        Thread server = new AcceptThread();
-        //server.run();
-    }
-
-    private void ask4Blu () {
-
-        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-
-        if (!bluetoothAdapter.isEnabled()) {
-            Intent enableBlueTooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBlueTooth, REQUEST_CODE_ENABLE_BLUETOOTH);
-        }
-
-    }
-
-
-    private final BroadcastReceiver bluetoothReceiver = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            Log.i("BAGARRE", "recherche de devices bluetooth ...");
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                Toast.makeText(BagarreActivity.this, "New Device = " + device.getName(), Toast.LENGTH_SHORT).show();
-                Log.i("BAGARRE", "nouveau device bluetooth : "+device.getName());
-                new ConnectThread(device);
-            }
-        }
-    };
-
-    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        //retrieve scan result
-        if (requestCode != REQUEST_CODE_ENABLE_BLUETOOTH)
-            if (resultCode == RESULT_OK) {
-                Log.i("BAGARRE", "bluetoot activé");
-            } else {
-                Log.i("BAGARRE", "bluetoot pas activé");
-            }
-    }
-
-    @Override
-    public void onClick(View v) {
-
-    }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        bluetoothAdapter.cancelDiscovery();
-        unregisterReceiver(bluetoothReceiver);
-    }
-
-    private class AcceptThread extends Thread {
-        private final BluetoothServerSocket mmServerSocket;
-
-        public AcceptThread() {
-            BluetoothServerSocket tmp = null;
-            try {
-                tmp = bluetoothAdapter.listenUsingRfcommWithServiceRecord(NAME, myUuid);
-            } catch (IOException e) { }
-            mmServerSocket = tmp;
-        }
-
-        public void run() {
-            BluetoothSocket socket = null;
-            while (true) {
-                try {
-                    socket = mmServerSocket.accept();
-                } catch (IOException e) {
-                    break;
-                }
-
-                if (socket != null) {
-                    try {
-                        runAsServer(socket);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                    try {
-                        mmServerSocket.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    break;
-                }
-            }
-        }
-
-        public void cancel() {
-            try {
-                mmServerSocket.close();
-            } catch (IOException e) { }
-        }
-    }
-    private class ConnectThread extends Thread {
-        private BluetoothSocket mmSocket = null;
-        private BluetoothDevice mmDevice;
-
-        public ConnectThread(BluetoothDevice device) {
-
-            Log.i("BAGARRE", "nouveau thread device bluetooth : "+device.getName());
-            BluetoothSocket tmp = null;
-            mmDevice = device;
-            try {
-                tmp = device.createRfcommSocketToServiceRecord(myUuid);
-            } catch (IOException e) { }
-            mmSocket = tmp;
-        }
-
-        public void run() {
-            bluetoothAdapter.cancelDiscovery();
-            try {
-                mmSocket.connect();
-            } catch (IOException connectException) {
-                try {
-                    mmSocket.close();
-                } catch (IOException closeException) { }
-                return;
-            }
-            try {
-                runAsClient(mmSocket);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
-
-        public void cancel() {
-            try {
-                mmSocket.close();
-            } catch (IOException e) { }
-        }
-
-    }
-
-    private class ConnectedThread extends Thread {
-        private final BluetoothSocket mmSocket;
-        private final InputStream mmInStream;
-        private final OutputStream mmOutStream;
-        private byte[] mmBuffer; // mmBuffer store for the stream
-
-        public ConnectedThread(BluetoothSocket socket) {
-            mmSocket = socket;
-            InputStream tmpIn = null;
-            OutputStream tmpOut = null;
-
-            // Get the input and output streams; using temp objects because
-            // member streams are final.
-            try {
-                tmpIn = socket.getInputStream();
-            } catch (IOException e) {
-                Log.e(TAG, "Error occurred when creating input stream", e);
-            }
-            try {
-                tmpOut = socket.getOutputStream();
-            } catch (IOException e) {
-                Log.e(TAG, "Error occurred when creating output stream", e);
-            }
-
-            mmInStream = tmpIn;
-            mmOutStream = tmpOut;
-        }
-
-        public void run() {
-            mmBuffer = new byte[1024];
-            int numBytes; // bytes returned from read()
-
-            // Keep listening to the InputStream until an exception occurs.
-            while (true) {
-                try {
-                    // Read from the InputStream.
-                    numBytes = mmInStream.read(mmBuffer);
-                    // Send the obtained bytes to the UI activity.
-                    Message readMsg = mHandler.obtainMessage(
-                            MessageConstants.MESSAGE_READ, numBytes, -1,
-                            mmBuffer);
-                    readMsg.sendToTarget();
-                    
-                    lancerBagarre(readMsg);
-                    
-                } catch (IOException e) {
-                    Log.d(TAG, "Input stream was disconnected", e);
-                    break;
-                }
-            }
-        }
-
-        // Call this from the main activity to send data to the remote device.
-        public void write(byte[] bytes) {
-            try {
-                mmOutStream.write(bytes);
-
-                // Share the sent message with the UI activity.
-                Message writtenMsg = mHandler.obtainMessage(
-                        MessageConstants.MESSAGE_WRITE, -1, -1, mmBuffer);
-                writtenMsg.sendToTarget();
-            } catch (IOException e) {
-                Log.e(TAG, "Error occurred when sending data", e);
-
-                // Send a failure message back to the activity.
-                Message writeErrorMsg =
-                        mHandler.obtainMessage(MessageConstants.MESSAGE_TOAST);
-                Bundle bundle = new Bundle();
-                bundle.putString("toast",
-                        "Couldn't send data to the other device");
-                writeErrorMsg.setData(bundle);
-                mHandler.sendMessage(writeErrorMsg);
-            }
-        }
-
-        // Call this method from the main activity to shut down the connection.
-        public void cancel() {
-            try {
-                mmSocket.close();
-            } catch (IOException e) {
-                Log.e(TAG, "Could not close the connect socket", e);
-            }
-        }
-    }
-
-    private void lancerBagarre(Message msg) {
-        fightTextView.setText("FIGHT");
-        String str = new String(msg.toString());
-        try {
-            JSONObject json = new JSONObject(str);
-            int monstreId = (int) json.get("monstreId");
-
+            //Création d'une instance de ma classe LivresBDD
             MonstreBDD monstreBDD = new MonstreBDD(this);
+
             monstreBDD.open();
 
-            monstreView.setImageResource(
-                    this.getResources().getIdentifier(monstreBDD.getMonstreWithID(monstreId).getApparence().toLowerCase(), "drawable", getPackageName()));
-            monstreBDD.close();
-        } catch (JSONException e) {
-            fightTextView.setText("ERROR JSON");
+            this.monstreJoueur = monstreBDD.getMonstreSelectionne();
+            if(monstreBDD.getArmeSelectionne() != null){
+                this.armeJoueur = monstreBDD.getArmeSelectionne();
+            }else{
+                this.armeJoueur = null;
+            }
+
+            if (monstreBDD.getArmureSelectionne() != null){
+                this.armureJoueur = monstreBDD.getArmureSelectionne();
+            }else{
+                this.armureJoueur = null;
+            }
+
+
+
+            this.nomMonstreJoueur.setText(monstreJoueur.getNom());
+            this.nomMonstreAdversaire.setText(monstreAFaireCombattre.getNom());
+
+
+            //On récupére tous les monstres pour en faire combattre un au hasard
+            ArrayList<Monstre> monstres = monstreBDD.getAllMonstres();
+
+            //On récupére toutes les armures pour en equiper une aléatoire
+            ArrayList<Armure> armures = monstreBDD.getAllArmures();
+            //idem pour les armes
+            ArrayList<Armure> armes = monstreBDD.getAllArmures();
+
+
+            Random rand = new Random();
+            //Renvoie un nombre en 0 et le nombre de monstres
+            int indexMonstre = rand.nextInt(monstres.size());
+
+            this.monstreAFaireCombattre = monstreBDD.getMonstreWithID(indexMonstre);
+            //On modifie ces points de vies en random
+            this.monstreAFaireCombattre.setPDV(rand.nextInt(50));
+            //idem pour les pda
+            this.monstreAFaireCombattre.setPDA(rand.nextInt(50));
+
+
+            int indexArmure = rand.nextInt(armures.size());
+            this.armureMonstreAFaireCombattre = monstreBDD.getArmureWithID(indexArmure);
+            this.armureMonstreAFaireCombattre.setDefense(rand.nextInt(50));
+
+            int indexArme = rand.nextInt(armes.size());
+            this.armeMonstreAFaireCombattre = monstreBDD.getArmeWithID(indexArme);
+            this.armeMonstreAFaireCombattre.setAttaque(rand.nextInt(50));
         }
     }
 
-    private void runAsServer(BluetoothSocket socket) throws JSONException {
-        ConnectedThread connection = new ConnectedThread(socket);
-        connection.run();
-        MonstreBDD monstreBDD = new MonstreBDD(this);
-        monstreBDD.open();
-        Monstre myMonstre = monstreBDD.getMonstreSelectionne();
-        Arme myArme = monstreBDD.getArmeSelectionne();
-        Armure myArmure = monstreBDD.getArmureSelectionne();
-        JSONObject json = new JSONObject();
-        json.put("monstreId", myMonstre.getId());
-        json.put("Name", myMonstre.getNom());
-        json.put("PDV", myMonstre.getPDV());
-        json.put("PDA", myMonstre.getPDA()+myArme.getAttaque());
-        json.put("def", myArmure.getDefense());
-        monstreBDD.close();
-        Message msg = obtain();
-        msg.obj = json;
-        connection.write(json.toString().getBytes());
+    //Méthode pour combattre flemme de la sortir du onclick x)
+    @Override
+    public void onClick(View v) {
+        Random rand = new Random();
+        int attMonstre = this.monstreJoueur.getPDA();
+        int attArme = 0;
+        if (this.armeJoueur != null){
+            attArme = this.armeJoueur.getAttaque();
+        }
+
+        int degat = rand.nextInt(attMonstre+attArme) - this.armureMonstreAFaireCombattre.getDefense();
+        this.infoView.setText("Vous infligez : " + degat);
+        //On fait subir des dégats au monstre
+        this.monstreAFaireCombattre.setPDV(monstreAFaireCombattre.getPDV()-degat);
+
+
+        if(this.monstreAFaireCombattre.getPDV()<=0){
+            this.barPVadversaire.setProgress(0);
+            this.infoView.append("\n Vous gagnez");
+            this.attaqueBTN.setEnabled(false);
+            return;
+        }else{
+            //On met à jour sa barre de vie
+            this.barPVadversaire.setProgress(monstreAFaireCombattre.getPDV());
+        }
+
+        //Au tour du joueur adversaire
+
+        attMonstre = monstreAFaireCombattre.getPDA();
+        attArme = this.armeMonstreAFaireCombattre.getAttaque();
+        if(this.armureJoueur != null){
+            degat = rand.nextInt(attMonstre+attArme) - this.armureJoueur.getDefense();
+        }else{
+            degat = rand.nextInt(attMonstre+attArme);
+        }
+
+        //On fait subir des dégats au monstre
+        this.monstreJoueur.setPDV(monstreJoueur.getPDV()-degat);
+
+
+        if(monstreJoueur.getPDV()<=0){
+            //On met à jour sa barre de vie
+            this.barPVJoueur.setProgress(0);
+            this.infoView.append("\n L'adversaire gagne");
+            this.attaqueBTN.setEnabled(false);
+        }else{
+            this.barPVJoueur.setProgress(monstreJoueur.getPDV());
+        }
     }
 
-    private void runAsClient(BluetoothSocket socket) throws JSONException {
-        ConnectedThread connection = new ConnectedThread(socket);
-        connection.run();
-        
+    //Fonction pour ne pas tout remettre à 0
+    public boolean alreadyInstance(){
+        if ((this.monstreJoueur != null) && (this.monstreAFaireCombattre != null)){
+            return true;
+        }
+        return false;
     }
+
 }
